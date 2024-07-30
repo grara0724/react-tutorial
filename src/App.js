654461a496f865df059247531e0e6ec3351a3c38
grara0724 <kazuma.xx24@gmail.com>
2024-07-31 01:19:58 +0900

    Reactで簡単なTodoアプリを作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // useStateは変数を監視するのに使われる
+  // todoの中にはname,id,completedのようなオブジェクトを格納する
+  // const[A,B] =useState();はAが更新された際に再レンダリングされる。(リロードされる)
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false },
+    { id: 2, name: "Todo2", completed: false },]);
+
+  // inputタイプの中にref={todoNameRef}と記載したものを取得できる
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    // タスクを追加する
+    const name = todoNameRef.current.value;
+
+  }
+
+  // リターンタグの一番外側にはdivを入れないと反映されない
+  return (<div>
+    {/* コンポーネントの部分にA={B}と記載することでAという名前でBを渡すという意味 */}
+    {/* このコンポーネントが読み込まれるところで受け取る必要もある */}
+    <TodoList todos={todos} />
+    <input type="text" ref={todoNameRef} />
+    <button onClick={handleAddTodo}>タスクを追加</button>
+    <button>完了したタスクの削除</button>
+    <div>残りのタスクを表示:0</div>
+  </div>);
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './Todo';
+
+// ここで（）の中身に好きな名前を割り当て、文中で｛｝を使って取り出せる
+// {}はこの中でJsを書けるということ<php?みたいなもの
+// 今回は({todes})で直接todosを渡している
+const TodoList = ({ todos }) => {
+    // mapは配列の中身を一つずつ取り出す
+    // 取り出した値をTodo.jsに送っている
+    // mapで送る際にはそれぞれのオブジェクトを識別するためのkeyをつける
+    return todos.map((todo) => <Todo todo={todo} key={todo} />);
+};
+
+export default TodoList;
