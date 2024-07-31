f2f8c9853bb1c80a91d1e5c75158a69f82d1fc5f
grara0724 <kazuma.xx24@gmail.com>
2024-07-31 22:35:21 +0900

    Reactを使ったtodoアプリの練習

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuid4 } from "uuid";
 
 function App() {
   // useStateは変数を監視するのに使われる
@@ -15,18 +16,48 @@ function App() {
   const handleAddTodo = () => {
     // タスクを追加する
     const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      // prevTodosに右側の値を追加する
+      // ...はスプレッド構文という
+      //       const foo = [1, 2];
 
+      // // 配列のクローン
+      // const bar = [...foo]; // => [1, 2]
+
+      // // 要素を追加して新しい配列を生成
+      // const baz = [...foo, 3, 4]; // => [1, 2, 3, 4]
+
+      // // 配列をマージ
+      // const hoge = [...foo, ...bar]; // => [1, 2, 1, 2]
+      return [...prevTodos, { id: uuid4(), name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  }
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    //todoにnweTodosの中から一つずつtodoとして取り出し、todo.idとidが一致していたらtodoに入れる
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed)
+    setTodos(newTodos);
   }
 
   // リターンタグの一番外側にはdivを入れないと反映されない
   return (<div>
     {/* コンポーネントの部分にA={B}と記載することでAという名前でBを渡すという意味 */}
     {/* このコンポーネントが読み込まれるところで受け取る必要もある */}
-    <TodoList todos={todos} />
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
     <input type="text" ref={todoNameRef} />
     <button onClick={handleAddTodo}>タスクを追加</button>
-    <button>完了したタスクの削除</button>
-    <div>残りのタスクを表示:0</div>
+    <button onClick={handleClear}>完了したタスクの削除</button>
+    {/* アロー関数でfilter関数でtodoのなかでコンプリートじゃないものの長さ */}
+    <div>残りのタスクを表示:{todos.filter((todo) => !todo.completed).length}</div>
   </div>);
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -4,11 +4,11 @@ import Todo from './Todo';
 // ここで（）の中身に好きな名前を割り当て、文中で｛｝を使って取り出せる
 // {}はこの中でJsを書けるということ<php?みたいなもの
 // 今回は({todes})で直接todosを渡している
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, toggleTodo }) => {
     // mapは配列の中身を一つずつ取り出す
     // 取り出した値をTodo.jsに送っている
     // mapで送る際にはそれぞれのオブジェクトを識別するためのkeyをつける
-    return todos.map((todo) => <Todo todo={todo} key={todo} />);
+    return todos.map((todo) => <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />);
 };
 
 export default TodoList;
